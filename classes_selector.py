faa7e1359a807c51eb136e006be4a9c231b63eda
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-08-25 10:53:51 +0530

    add ImageNet100 to class_selector

--- classes_selector.py
+++ classes_selector.py
@@ -3,7 +3,9 @@ from typing import List
 import numpy as np
 np.random.seed(42)
 
-def synset_selector(filename: str='./LOC_synset_mapping.txt', debug:bool=False) -> List[int]:
+
+# Randomly samples 15 synsets from ImageNet
+def synset_selector1k(filename: str='./LOC_synset_mapping.txt', debug:bool=False) -> List[int]:
     with open(filename) as f:
         lines = [line.strip() for line in f.readlines()]
         classes = np.random.randint(low=0, high=1000, size=(15,))
@@ -16,7 +18,8 @@ def synset_selector(filename: str='./LOC_synset_mapping.txt', debug:bool=False) 
             print()
         return classes.tolist()
 
-def class_selector(filename: str='./LOC_synset_mapping.txt', debug:bool=False) -> List[int]:
+# Randomly samples 15 synsets with only a single class per synset from ImageNet
+def class_selector1k(filename: str='./LOC_synset_mapping.txt', debug:bool=False) -> List[int]:
     with open(filename) as f:
         lines = [line.strip() for line in f.readlines()]
         classes = []
@@ -35,7 +38,40 @@ def class_selector(filename: str='./LOC_synset_mapping.txt', debug:bool=False) -
             print()
         return classes
 
+# Randomly samples 15 synsets from ImageNet100
+def synset_selector100(filename: str='./imagenet100labels.txt', debug:bool=False) -> List[int]:
+    with open(filename) as f:
+        lines = [line.strip() for line in f.readlines()]
+        classes = np.random.randint(low=0, high=100, size=(15,))
+        if debug:
+            print(f"(class_id, synset_id, synset)")
+            print()
+            for id in classes:
+                print(id, end="\t")
+                print(f"{lines[int(id)][:9]}\t{lines[int(id)][9:]}")
+            print()
+        return classes.tolist()
+
+# Randomly samples 15 synsets with a single class per synset from ImageNet100
+def class_selector100(filename: str='./LOC_synset_mapping.txt', debug:bool=False) -> List[int]:
+    with open(filename) as f:
+        lines = [line.strip() for line in f.readlines()]
+        classes = []
+        while len(classes) < 15:
+            idx = random.randint(0, 100)
+            mapping = lines[idx]
+            if ',' in mapping:
+                continue
+            classes.append(idx)
+        if debug:
+            print(f"(class_id, synset_id, synset)")
+            print()
+            for id in classes:
+                print(id, end="\t")
+                print(f"{lines[int(id)][:9]}\t{lines[int(id)][9:]}")
+            print()
+        return classes
 
 if __name__ == "__main__":
-    class_selector(debug=True)
-    synset_selector(debug=True)
+    synset_selector100(debug=True)
+    class_selector100(debug=True)
